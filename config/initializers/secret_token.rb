0f5c48764b751792f11005d97dec05573632f312
josecortes <jcortes178@hotmail.com>
2011-11-18 18:29:17 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaltersConstruction::Application.config.secret_token = '7ad7bb585034eecbbc763b3cb53545418b772805ec580fbcf3fe74dc9917dc9ca0d0e5f8e4d9467b71096bf372df94a83b1406df72a658f3342ec3fbfa56dc2d'
