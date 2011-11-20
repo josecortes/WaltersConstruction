4e30ebc1b5b4f5c6d22466866c864908fd65ee54
josecortes <jcortes178@hotmail.com>
2011-11-19 18:06:56 -0600

    Starting to use the application layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,15 @@
 class PagesController < ApplicationController
   def login
-   @title = "Walters Construction Home"
+   @title = "Walters Construction Login"
    
   end
 
   def emp_home
+    @title = "Employee Home"
   end
 
   def emp_hours
+    @title = "Employee Hours Entry"
   end
 
 end
