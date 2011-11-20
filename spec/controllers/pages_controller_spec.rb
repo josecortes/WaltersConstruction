4a5101a369ddcfe031a11e5cd707031e48a72c7b
josecortes <jcortes178@hotmail.com>
2011-11-19 18:33:33 -0600

    Making things more dynamic

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,7 +12,7 @@ describe PagesController do
     it "home should have the right title" do
       get 'login'
       response.should have_selector("title",
-        :content=> "Walters Construction Home")
+        :content=> "Walters Construction Login")
     end
   end
   
