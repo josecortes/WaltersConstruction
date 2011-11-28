ce06e5b0dd30fb424097fccedf2ed1d79f241b1a
josecortes <jcortes178@hotmail.com>
2011-11-28 15:57:08 -0600

    generating tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password
+  attr_accessible :name, :email, :password, :position
+  
+#  validates :name, :presence => true
+#  validates :password, :presence => true
+#  validates :email, :presence => true
+#  validates :position, :presence => true
 end
